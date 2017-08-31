7992e04512416eb06123fa52078f7df65cfeb405
Ting Zhou <zhouting@umich.edu>
2017-08-31 10:10:19 -0400

    Update

--- UMTRI_data_sync.py
+++ UMTRI_data_sync.py
@@ -27,76 +27,75 @@ def get_last_row(csv_filename):
         return lastrow
 
 
-def load():
-    # Create output dir if not exist
-    if not os.path.exists('./output'):
-        os.makedirs('./output')
+# Create output dir if not exist
+if not os.path.exists('./output'):
+    os.makedirs('./output')
 
-    # Video file ** Change video start time here **
-    video_start_time = video_start_datetime.split()[1]
-    video_end_time = video_end_datetime.split()[1]
+# Video file ** Change video start time here **
+video_start_time = video_start_datetime.split()[1]
+video_end_time = video_end_datetime.split()[1]
 
-    # Convert the video start time to seconds
-    video_start_time_seconds = time2seconds(video_start_time)
-    video_end_time_seconds = time2seconds(video_end_time)
+# Convert the video start time to seconds
+video_start_time_seconds = time2seconds(video_start_time)
+video_end_time_seconds = time2seconds(video_end_time)
 
-    # Datalogger file #############################################################
-    datalogger_output_path = './output/synced_' + datalogger_input_path[2:]
+# Datalogger file #############################################################
+datalogger_output_path = './output/synced_' + datalogger_input_path[2:]
 
-    datalogger_start_time = ''
-    datalogger_start_time_seconds = 0
+datalogger_start_time = ''
+datalogger_start_time_seconds = 0
 
-    # delta time between video_start_time and datalogger_start_time
-    delta_time_datalogger = 0
-    with open(datalogger_input_path, 'r') as inp, open(
-            datalogger_output_path, 'w', newline='') as out:
-        writer = csv.writer(out)
-        for line_num, row in enumerate(csv.reader(inp), 1):
-            # retrieve the datalogger start time and convert to seconds
-            if line_num == 3:
-                idx = row[0].find(':')
-                cur_time = row[0][idx + 1:].strip()
-                time_arr = cur_time.split()
-                if time_arr[2] == 'PM':
-                    hms = time_arr[1].split(':')
-                    hms[0] = str(int(hms[0]) + 12)
-                    datalogger_start_time = ':'.join(hms)
-                else:
-                    datalogger_start_time = time_arr[2]
-                datalogger_start_time_seconds = time2seconds(
-                    datalogger_start_time)
-            elif line_num > 3:
-                break
-
-    datalogger_lastrow = get_last_row(datalogger_input_path)
-    datalogger_end_time_seconds = datalogger_start_time_seconds + float(
-        datalogger_lastrow[0])
-
-    # Bioharness file #############################################################
-    bioharness_output_path = './output/synced_' + bioharness_input_path[2:]
-    with open(bioharness_input_path, 'r') as f:
-        lastrow = None
-        firstrow = None
-        for line_num, row in enumerate(csv.reader(f), 1):
-            if line_num == 2:
-                firstrow = row
+# delta time between video_start_time and datalogger_start_time
+delta_time_datalogger = 0
+with open(datalogger_input_path, 'r') as inp, open(
+        datalogger_output_path, 'w', newline='') as out:
+    writer = csv.writer(out)
+    for line_num, row in enumerate(csv.reader(inp), 1):
+        # retrieve the datalogger start time and convert to seconds
+        if line_num == 3:
+            idx = row[0].find(':')
+            cur_time = row[0][idx + 1:].strip()
+            time_arr = cur_time.split()
+            if time_arr[2] == 'PM':
+                hms = time_arr[1].split(':')
+                hms[0] = str(int(hms[0]) + 12)
+                datalogger_start_time = ':'.join(hms)
             else:
-                pass
-        lastrow = row
-    bioharness_start_time = firstrow[0].split()[1].split('.')[0]
-    bioharness_start_time_seconds = time2seconds(bioharness_start_time)
-    bioharness_end_time = lastrow[0].split()[1].split('.')[0]
-    bioharness_end_time_seconds = time2seconds(bioharness_end_time)
+                datalogger_start_time = time_arr[2]
+            datalogger_start_time_seconds = time2seconds(
+                datalogger_start_time)
+        elif line_num > 3:
+            break
 
-    # Decide the time period!!! ###################################################
-    start_time = max(video_start_time_seconds, datalogger_start_time_seconds,
-                     bioharness_start_time_seconds)
-    end_time = min(video_end_time_seconds, datalogger_end_time_seconds,
-                   bioharness_end_time_seconds)
-    duration = end_time - start_time
+datalogger_lastrow = get_last_row(datalogger_input_path)
+datalogger_end_time_seconds = datalogger_start_time_seconds + float(
+    datalogger_lastrow[0])
 
-    delta_time_datalogger = start_time - datalogger_start_time_seconds
-    delta_time_bioharness = start_time - bioharness_start_time_seconds
+# Bioharness file #############################################################
+bioharness_output_path = './output/synced_' + bioharness_input_path[2:]
+with open(bioharness_input_path, 'r') as f:
+    lastrow = None
+    firstrow = None
+    for line_num, row in enumerate(csv.reader(f), 1):
+        if line_num == 2:
+            firstrow = row
+        else:
+            pass
+    lastrow = row
+bioharness_start_time = firstrow[0].split()[1].split('.')[0]
+bioharness_start_time_seconds = time2seconds(bioharness_start_time)
+bioharness_end_time = lastrow[0].split()[1].split('.')[0]
+bioharness_end_time_seconds = time2seconds(bioharness_end_time)
+
+# Decide the time period!!! ###################################################
+start_time = max(video_start_time_seconds, datalogger_start_time_seconds,
+                 bioharness_start_time_seconds)
+end_time = min(video_end_time_seconds, datalogger_end_time_seconds,
+               bioharness_end_time_seconds)
+duration = end_time - start_time
+
+delta_time_datalogger = start_time - datalogger_start_time_seconds
+delta_time_bioharness = start_time - bioharness_start_time_seconds
 
 def datalogger_sync():
     # ========================= Datalogger Sync Start ========================
@@ -161,7 +160,6 @@ def video_clip():
     # ========================= Video Clip end ===========================
 
 def main():
-    load()
     datalogger_sync()
     bioharness_sync()
     video_clip()
